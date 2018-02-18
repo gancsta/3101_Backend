3a1360f3bbc1dded09e843291d4b77c27caef29f
Denys Stasyuk <gancsta@ukr.net>
2018-02-18 14:01:50 +0200

    add file 28.php

---
+++ arrays_loops_tasks/28.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+	<title>Генератор случайных (заданных) таблиц</title>
+</head>
+<body>
+	<div>Вывести таблицу умножения с помощью двух циклов for.</div>	
+	<div>Смотреть файл /arrays_loops_tasks/13.php</div>	
+</body>
+</html>	
